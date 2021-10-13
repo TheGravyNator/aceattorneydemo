c21dee09446660f91f0782771fac7c1a3c292af8
Christian Slabbekoorn <cpc.slabbekoorn@gmail.com>
2021-10-13 11:33:53 +0200

    Added refactor action comments to Storymanager.cs

--- Ace Attorney Demo/Assets/Scripts/Story/StoryManager.cs
+++ Ace Attorney Demo/Assets/Scripts/Story/StoryManager.cs
@@ -8,40 +8,49 @@ using UnityEngine.UI;
 
 public class StoryManager : MonoBehaviour
 {
+    // Move to InkParsingHandler
     public Story story;
     public TextAsset inkFile;
-
+     
     public TextboxController textBox;
-
+    public TextboxController CrossExaminationTextBox;
+    
+    // Move to StoryUIHandler
     public GameObject CrossExaminationUI;
     public GameObject InGameUI;
-    public TextboxController CrossExaminationTextBox;
-
     public Image FadeScreen;
-
+    
+    // Subscribe MusicManager to event
     public MusicManager music;
 
+    // Move to StorySFXHandler
     public AudioSource sfx;
     public AudioSource exclamationSfx;
 
     public AudioClip ObjectionSfx;
     public AudioClip HoldItSfx;
-
+    
+    // Move to StoryUIHandler
     public GameObject ObjectionUI;
     public GameObject HoldItUI;
-
+    
+    // Related to the bools in the Update function, rework that
     public InventoryManager inventory;
     
+    // Change some things regarding this event, refer to AdvanceDialogue comment
     public delegate void DialogueChanged(string name, string emote);
     public static event DialogueChanged OnDialogueChanged;
 
+    // Figure out what to do with this title bool
     private bool title = true;
+    
     private string evidence;
 
     List<Choice> choices;
 
     void Start()
     {
+        // Move to InkParserHandler
         story = new Story(inkFile.text);
 
         CrossExaminationStart();
@@ -49,10 +58,13 @@ public class StoryManager : MonoBehaviour
     
     void Update()
     {
+        // Find a better solution to work this out
         if (!CrossExaminationTextBox.writeTitle)
         {
             StartTestimony();
         }
+        
+        // What's going on with all these bools? Can I handle this better?
         if (Input.GetKeyDown(KeyCode.Space) && textBox.canWrite && !inventory.isMenuActive && !title)
         {
             if (choices != null && choices.Count >= 3)
@@ -64,11 +76,13 @@ public class StoryManager : MonoBehaviour
         }
     }
 
+    // You're fine, bud
     private void StoryEnd()
     {
         SceneManager.LoadScene("Main Menu");
     }
-
+    
+    // Move to StoryUIHandler
     IEnumerator FadeToBlack()
     {
         for (float alpha = 0f; FadeScreen.color.a < 1f; alpha += 0.1f)
@@ -81,12 +95,16 @@ public class StoryManager : MonoBehaviour
 
     private void CrossExaminationStart()
     {
+        // Move to StoryUIHandler
         CrossExaminationUI.GetComponentInChildren<Animation>().Play();
+        CrossExaminationTextBox.WriteTitle(title);
+        
+        // Move to InkParsingHandler
         string title = story.Continue();
         evidence = story.currentTags[0];
-        CrossExaminationTextBox.WriteTitle(title);
     }
 
+    // Figure out how to do this better
     private void StartTestimony()
     {
         if (title)
@@ -98,19 +116,25 @@ public class StoryManager : MonoBehaviour
 
     public void AdvanceDialogue()
     {
+        // Move to StoryUIHandler
         CrossExaminationUI.SetActive(false);
         InGameUI.SetActive(true);
+        
         var line = GetNextLine();
+        
+        // Rewrite the event on this, move textbox call to StoryUIHandler
         textBox.WriteText(line.name, line.text);
         OnDialogueChanged(line.name, line.tag);
     }
-
+    
+    // Move to InkParserHandler
     public void MakeChoice(int index)
     {
         Choice choiceSelected = choices[index];
         story.ChooseChoiceIndex(choiceSelected.index);
     }
 
+    // Move to InkParserHandler
     private (string name, string text, string tag) GetNextLine()
     {
         textBox.textField.color = Color.white;
@@ -127,7 +151,8 @@ public class StoryManager : MonoBehaviour
         string tag = tags[0];
         return (lineAndName[0], lineAndName[1].TrimStart(' '), tag);
     }
-
+    
+    // Create event for this
     public void PressStatement()
     {
         if (choices != null && choices.Count >= 3)
@@ -136,6 +161,7 @@ public class StoryManager : MonoBehaviour
         }
     }
 
+    // Create event for this
     public void PresentEvidence(Clue item)
     {
         if (choices.Count == 4 && item.name == evidence)
@@ -148,7 +174,8 @@ public class StoryManager : MonoBehaviour
             StartCoroutine(Objection(false));
         }
     }
-
+    
+    // Move to StoryUIManager and StorySFXManager
     IEnumerator Objection(bool right)
     {
         exclamationSfx.PlayOneShot(ObjectionSfx);
@@ -169,6 +196,7 @@ public class StoryManager : MonoBehaviour
         AdvanceDialogue();
     }
 
+    // Move to StoryUIManager and StorySFXManager
     IEnumerator HoldIt()
     {
         exclamationSfx.PlayOneShot(HoldItSfx);
